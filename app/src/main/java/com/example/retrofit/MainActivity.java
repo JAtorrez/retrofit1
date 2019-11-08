dee6947e9830bccc374bead71520df0e5168d94f
jose <pepe17199@hotmail.com>
2019-11-07 23:09:54 -0600

    2do guardado

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.retrofit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,18 +19,27 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView mytexteview;
+    private TextView txtsalida;
+    private Button btn;
+    private CheckBox c1, c2 , c3, c4;
+    private String content = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mytexteview = findViewById(R.id.test);
-        getPost();
+        txtsalida = findViewById(R.id.salida);
+        btn = findViewById(R.id.btnpeticion);
+        btn.setOnClickListener(this);
+        c1 = findViewById(R.id.cbmuseo);
+        c2 = findViewById(R.id.cbpark);
+        c3 = findViewById(R.id.cbrest);
+        c4 = findViewById(R.id.cbschool);
     }
 
-    private void getPost(){
+    private void getdata(){
         Retrofit retrofit = new  Retrofit.Builder().baseUrl("https://maps.googleapis.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
@@ -41,25 +53,76 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
 
                 if(!response.isSuccessful()){
-                    mytexteview.setText("Codigo: "+response.code());
+                    txtsalida.setText("Codigo: "+response.code());
                     return;
                 }
                 Respuesta respuesta = response.body();
+                int i = 1;
 
                 for(Result result: respuesta.getResults() ){
-                    String content = "";
-                    content += "Name: "+ result.getName()+"\n";
+                    content = "";
+                    content += i + result.getName()+", ";
                     content += "Rating: "+ result.getRating()+"\n";
 
-                    mytexteview.append(content);
+                    txtsalida.append(content);
+                    i++;
                }
+                btn.setEnabled(true);
 
             }
 
             @Override
             public void onFailure(Call<Respuesta> call, Throwable t) {
-                mytexteview.setText(t.getMessage());
+                txtsalida.setText(t.getMessage());
             }
         });
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+
+            case R.id.btnpeticion:
+                btn.setEnabled(false);
+                content = "";
+                txtsalida.setText(content);
+                if (c1.isChecked()){
+                    String tipo = "museum";
+                    getdata();
+                }
+                if (c2.isChecked()){
+                    String tipo = "park";
+                    getdata();
+                }
+                if (c3.isChecked()){
+
+                    String tipo = "restaurant";
+                    getdata();
+
+                }
+                if (c4.isChecked()){
+
+                    String tipo = "school";
+                    getdata();
+
+                }else if(!c1.isChecked() && !c2.isChecked() && !c3.isChecked() && !c4.isChecked()  ) {
+                    content = "Por favor \n seleccione una opcion.";
+                    txtsalida.setText(content);
+                    btn.setEnabled(true);
+                    content = "";
+
+                }
+
+
+                break;
+            default:
+
+                break;
+        }
+
+
+    }
+
 }
